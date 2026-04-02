eeaf9f3edd7eda3c7919432530977a041bf0e4a3
Yamson7090 <2627315243@qq.com>
2026-04-02 22:28:44 +0800

    deleted:    config.yml

--- definitions.py
+++ definitions.py
@@ -6,5 +6,9 @@ def load_config():
             config = yaml.safe_load(file)
         return config
     except FileNotFoundError:
-        print("❌ 错误：找不到 config.yml 文件，已重新生成默认配置。")
+        with open('config.yml', 'w', encoding='utf-8') as file:
+            with open('defaults/default_config.yml', 'r', encoding='utf-8') as default_file:
+                default_config = default_file.read()
+            file.write(default_config)
+        print("❌ 错误：找不到 config.yml 文件，已重新生成默认配置，请根据文件内容进行修改。")
         exit(1)
