ac82796efb0c8ba52f0f1ee4862d027062c15381
Yamson7090 <2627315243@qq.com>
2026-04-03 12:34:00 +0800

    modified:   definitions.py

--- definitions.py
+++ definitions.py
@@ -1,4 +1,6 @@
 import yaml
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def load_config():
     try:
@@ -12,3 +14,7 @@ def load_config():
             file.write(default_config)
         print("❌ 错误：找不到 config.yml 文件，已重新生成默认配置，请根据文件内容进行修改。")
         exit(1)
+
+def sqlite_connect(db_name='users.db'):
+    conn = sqlite3.connect(db_name)
+    return conn
